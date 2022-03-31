4c51bf4ce28bb2ec095c94844da1f49f27faabfa
Arthaudcom <arthaudmorin@gmail.com>
2022-03-31 16:48:48 -0400

    envoi des buches d'endroit differents

--- Assets/Scripts/LogLauncher.cs
+++ Assets/Scripts/LogLauncher.cs
@@ -12,12 +12,29 @@ public class LogLauncher : MonoBehaviour
     public float interpolationPeriod;
     private float time2 = 2.0f;
     private List<GameObject> liste = new List<GameObject>();
- 
+    private List<GameObject> liste2 = new List<GameObject>();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    Vector3 Fly(Vector3 logPos)
+    {
+        Vector3 vel = camPos.position - logPos;
+
+        float x, y, z;
+        x = (vel.x / 1.9f) * 50;//vel.y * 20;
+        y = (10f) * 50;
+        z = (vel.z / 1.9f) * 50;
+
+        vel.y = y;
+        vel.x = Random.Range(x - 15, x + 15);
+        vel.z = Random.Range(z - 15, z + 15);
+
+        return vel;
     }
 
     // Update is called once per frame
@@ -30,28 +47,61 @@ public class LogLauncher : MonoBehaviour
         {
             time += interpolationPeriod;
 
+            var rnd = new System.Random();
+            int Buche = rnd.Next(1, 4);  // creates a number between 1 and 3
+
+            switch (Buche)
+            {
+                case 1:
+                    GameObject newBuche = Instantiate(monPrefab, new Vector3(133, 0.5f, 81), Quaternion.identity);
+                    liste.Add(newBuche);
+                    Rigidbody rb = newBuche.GetComponent<Rigidbody>();
+                    rb.AddForce(Fly(rb.transform.position));
+                    rb.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+                    break;
+
+                case 2:
+                    GameObject newBuche2 = Instantiate(monPrefab, new Vector3(140, 0.5f, 90), Quaternion.identity);
+                    liste.Add(newBuche2);
+                    Rigidbody rb2 = newBuche2.GetComponent<Rigidbody>();
+                    rb2.AddForce(Fly(rb2.transform.position));
+                    rb2.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+                    break;
+
+                case 3:
+                    GameObject newBuche3 = Instantiate(monPrefab, new Vector3(140, 0.5f, 70), Quaternion.identity);
+                    liste.Add(newBuche3);
+                    Rigidbody rb3 = newBuche3.GetComponent<Rigidbody>();
+                    rb3.AddForce(Fly(rb3.transform.position));
+                    rb3.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+                    break;
+                    break;
+            }
+
+            /*
             GameObject newBuche = Instantiate(monPrefab, new Vector3(133, 0.5f, 81), Quaternion.identity);
             liste.Add(newBuche);
+            GameObject newBuche2 = Instantiate(monPrefab, new Vector3(140, 0.5f, 90), Quaternion.identity);
+            liste.Add(newBuche2);
+            GameObject newBuche3 = Instantiate(monPrefab, new Vector3(140, 0.5f, 70), Quaternion.identity);
+            liste.Add(newBuche3);
             Rigidbody rb = newBuche.GetComponent<Rigidbody>();
+            Rigidbody rb2 = newBuche2.GetComponent<Rigidbody>();
+            Rigidbody rb3 = newBuche3.GetComponent<Rigidbody>();
             //rb.AddForce(Random.Range(-23,-23), Random.Range(10,10), Random.Range(-5, -5), ForceMode.Impulse);
 
-            Vector3 vel = camPos.position - rb.transform.position;
-            float x, y, z;
-            x = (vel.x / 1.9f)*50;//vel.y * 20;
-            y = (10f)*50;
-            z = (vel.z / 1.9f)*50;
 
-            vel.y = y;
-            vel.x = Random.Range(x-15, x+15);
-            vel.z = Random.Range(z-15, z+15);
-
-            rb.AddForce(vel);
+            rb.AddForce(Fly(rb.transform.position));
+            rb2.AddForce(Fly(rb2.transform.position));
+            rb3.AddForce(Fly(rb3.transform.position));
 
             //rb.velocity = vel;
 
             rb.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+            rb2.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+            rb3.AddTorque(Random.Range(-20, 20), 0, Random.Range(-20, 20));*/
 
-            if(current > interpolationPeriod * 3)
+            if (current > interpolationPeriod * 3)
             {
                 time2 += 2 * interpolationPeriod;
                 GameObject o = liste[0];
